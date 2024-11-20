44bb5140596689f4effb66cf5a858a291f2df9a8
Guy Coop <gcoop@outlook.com>
2024-11-20 22:08:36 +0000

    inital recipe selector

--- lib/interface/pages/recipe_selector.py
+++ lib/interface/pages/recipe_selector.py
@@ -1,5 +1,8 @@
 import tkinter as tk
-
+from lib.backend.recipe import load_all_recipes, Recipe
+from PIL import Image, ImageTk
+from pathlib import Path
+ASSETS_PATH = Path(__file__).parent.parent.parent.parent / "recipes" / "assets"
 
 class RecipeSelector(tk.Frame):
     def __init__(self, parent, controller, *args, **kwargs):
@@ -9,14 +12,34 @@ class RecipeSelector(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
         button_home = tk.Button(
             self,
-            text="Go to Start Page",
+            text="Back to Home",
             command=lambda: controller.show_frame("StartPage"),
         )
         button_home.pack()
 
-        button_ingredient = tk.Button(
-            self,
-            text="Go to Ingredient Display",
-            command=lambda: controller.show_frame("IngredientDisplay"),
-        )
-        button_ingredient.pack()
+        # load all the recipes from file and display them as squares 250x250 with the recipe name, a tick box, and an image if the recipe contains the "image" key
+        self.recipes = load_all_recipes()
+
+        # display the recipes in a grid 2 wide
+        for recipe in self.recipes.values():
+            recipe_frame = tk.Frame(self)
+            recipe_frame.pack()
+            recipe_label = tk.Label(recipe_frame, text=recipe.name)
+            recipe_label.pack()
+            recipe_image_fname = recipe.image if recipe.image else "default.png"
+
+            # if the image is not 1:1 aspect ratio then crop it to 1:1
+            recipe_image = Image.open(ASSETS_PATH / recipe_image_fname)
+            if recipe_image.width != recipe_image.height:
+                recipe_image = recipe_image.crop((0, 0, min(recipe_image.width, recipe_image.height), min(recipe_image.width, recipe_image.height)))
+            recipe_image.thumbnail((250, 250))
+            recipe_image = ImageTk.PhotoImage(recipe_image)
+            recipe_image_label = tk.Label(recipe_frame, image=recipe_image)
+            recipe_image_label.image = recipe_image
+            recipe_image_label.pack()
+            recipe_checkbox = tk.Checkbutton(recipe_frame)
+            recipe_checkbox.pack()
+
+
+
+
